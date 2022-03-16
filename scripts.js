eb79424159f86dd167253a9830bcac62cb91c89b
EmmaK <emmaculatewkamau@gmail.com>
2022-03-16 14:49:02 +0300

    Add centered images

--- scripts.js
+++ scripts.js
@@ -5,12 +5,16 @@ $(document).ready(function(){
     $("button.title").click(function(e){
         let elem = `p#${e.target.value}`
         let newElem = elem.split("#")
+        let newImg = "img-"+newElem[1]
         var showDesc = document.getElementById(newElem[1]) //What we want to show
+        console.log(newImg)
         
         if(showDesc.style.display === "block"){
             showDesc.style.display = "none"
         }else{
             $("p").hide()
+            $("img").addClass("centered")
+            $("img#"+newImg).removeClass("centered")
             showDesc.style.display = "block" //Display selected button
         }
     })
